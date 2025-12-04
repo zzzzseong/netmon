a2454dccc307562f6754e61a5fecc86998c27cb3
zzzzseong <jung48182@gmail.com>
2025-12-04 15:09:54 +0900

    fix: remove completion command

--- cmd/root.go
+++ cmd/root.go
@@ -34,4 +34,7 @@ func init() {
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.netmon.yaml)")
+	
+	// Disable completion command completely (official Cobra way)
+	rootCmd.CompletionOptions.DisableDefaultCmd = true
 }
